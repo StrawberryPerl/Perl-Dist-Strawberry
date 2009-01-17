b65e934d58acf41ea23c3949dfb204b411a55531
Adam Kennedy <adamk@cpan.org>
2009-01-17 13:30:05 +0000

    Updating copyright

--- lib/Perl/Dist/Strawberry.pm
+++ lib/Perl/Dist/Strawberry.pm
@@ -587,7 +587,7 @@ Adam Kennedy E<lt>adamk@cpan.orgE<gt>
 
 =head1 COPYRIGHT
 
-Copyright 2007 - 2008 Adam Kennedy.
+Copyright 2007 - 2009 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
