08a2a236a5165849617f4537005884cbdc402e54
Curtis Jewell <csjewell@cpan.org>
2010-04-22 04:27:24 +0000

    Turning off building of betas.

--- lib/Perl/Dist/Strawberry.pm
+++ lib/Perl/Dist/Strawberry.pm
@@ -220,7 +220,7 @@ sub new {
 		
 		# Program version.
 		build_number         => 2,
-		beta_number          => 2,
+#		beta_number          => 0,
 		
 		# New options for msi building...
 		msi_license_file     => $dist_dir->file('License-short.rtf'),
