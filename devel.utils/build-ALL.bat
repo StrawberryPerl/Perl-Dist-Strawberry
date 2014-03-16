c296cd8dd1b59674f4e9ad9a1d71217d3f6f41d9
KMX <kmx@cpan.org>
2014-03-16 19:29:48 +0000

    tuning build scripts

--- devel.utils/build-ALL.bat
+++ devel.utils/build-ALL.bat
@@ -1,5 +1,8 @@
 @cls
 call ..\build.bat test
+perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.19.9.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
+perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\64bit-5.19.9.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
+
 perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.16.3.1-SPP-min.pp -nointeractive -norestorepoints
 perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.16.3.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
 perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\64bit-5.16.3.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
@@ -7,7 +10,4 @@ perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -jo
 perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.18.2.1-no64.pp    -nointeractive -norestorepoints
 perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.18.2.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
 perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\64bit-5.18.2.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
-
-perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.19.9.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
-perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\64bit-5.19.9.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
 pause
