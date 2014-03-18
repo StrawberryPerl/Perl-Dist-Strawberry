e5c71b265586e2aff3087ad9ee7366f39b5fabfd
KMX <kmx@cpan.org>
2014-03-18 14:49:40 +0000

    build 5.19.9 tuning

--- devel.utils/build-5.20.bat
+++ devel.utils/build-5.20.bat
@@ -1,6 +1,6 @@
 @cls
 call ..\build.bat test
-perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.19.9.1.pp         -interactive -restorepoints -wixbin_dir=z:\sw\Wix38
-rem perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.19.9.1-no64.pp    -nointeractive -norestorepoints
 perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\64bit-5.19.9.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
+perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.19.9.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
+rem perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.19.9.1-no64.pp    -nointeractive -norestorepoints
 pause
