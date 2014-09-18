0c5b4092160230398815178de58c9d98d2e038d3
kmx <kmx@cpan.org>
2014-09-18 09:38:27 +0200

    preparing for 5.18.3

--- devel.utils/build-5.18.bat
+++ devel.utils/build-5.18.bat
@@ -1,6 +1,6 @@
 @cls
 call ..\build.bat test
-perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\64bit-5.18.2.2.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
-perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.18.2.2.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
-perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.18.2.2-no64.pp    -nointeractive -norestorepoints
+perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.18.3.1.pp       -test_core -cpan_url file:///z:/_cpan-mirror/ -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
+perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.18.3.1.pp       -test_core -cpan_url file:///z:/_cpan-mirror/ -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
+perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.18.3.1-no64.pp  -test_core -cpan_url file:///z:/_cpan-mirror/ -nointeractive -norestorepoints
 pause
