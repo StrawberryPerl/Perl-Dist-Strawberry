180a383a4eab521efdfb78129ca6e0fcd727475b
kmx <kmx@cpan.org>
2018-04-01 19:41:06 +0200

    updated openssl for 5.26.2.1

--- share/64bit-5.26.2.1.pp
+++ share/64bit-5.26.2.1.pp
@@ -47,7 +47,7 @@
             'liblibxslt'    => '<package_url>/kmx/64_libs/gcc71-2017Q2/64bit_libxslt-1.1.29-bin_20170517.zip',
             'libmpc'        => '<package_url>/kmx/64_libs/gcc71-2017Q2/64bit_mpc-1.0.3-bin_20170517.zip',
             'libmpfr'       => '<package_url>/kmx/64_libs/gcc71-2017Q2/64bit_mpfr-3.1.5-bin_20170517.zip',
-            'libopenssl'    => '<package_url>/kmx/64_libs/gcc71-2017Q2/64bit_openssl-1.1.0f-bin_20170529.zip',
+            'libopenssl'    => '<package_url>/kmx/64_libs/gcc71-2018Q2/64bit_openssl-1.1.0h-bin_20180331.zip',
             'libpostgresql' => '<package_url>/kmx/64_libs/gcc71-2017Q2/64bit_postgresql-9.6.3-bin_20170517.zip',
             'libt1lib'      => '<package_url>/kmx/64_libs/gcc71-2017Q2/64bit_t1lib-5.1.2-bin_20170517.zip',
             'libtiff'       => '<package_url>/kmx/64_libs/gcc71-2017Q2/64bit_tiff-4.0.7-bin_20170517.zip',
