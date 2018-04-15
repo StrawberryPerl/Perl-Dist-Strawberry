910de8bae6a7849656514cc0dd01a2e429bba071
kmx <kmx@cpan.org>
2018-04-15 11:41:15 +0200

    update build bat

--- devel.utils/build-5.24.bat
+++ devel.utils/build-5.24.bat
@@ -14,10 +14,11 @@ set SKIP_PDL_STEP=1
 set SKIP_LD_TROUBLE_MAKERS=
 perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.24.4.1.pp -notest_core -beta=0 -noperl_64bitint -app_simplename=strawberry-perl-no64 -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
 
-set SKIP_MSI_STEP=1
-set SKIP_PDL_STEP=
-set SKIP_LD_TROUBLE_MAKERS=1
-perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.24.4.1.pp -notest_core -beta=0 -perl_ldouble -app_simplename=strawberry-perl-ld -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
+rem ### no long double build ###
+rem ### set SKIP_MSI_STEP=1
+rem ### set SKIP_PDL_STEP=
+rem ### set SKIP_LD_TROUBLE_MAKERS=1
+rem ### perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.24.4.1.pp -notest_core -beta=0 -perl_ldouble -app_simplename=strawberry-perl-ld -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
 
 rem ### EXPERIMENTAL ###
 rem ### perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.24.4.1.pp -test_core -beta=0 -perl_ldouble -noperl_64bitint -app_simplename=strawberry-PDL-ld-no64 -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
--- devel.utils/build-5.26.bat
+++ devel.utils/build-5.26.bat
@@ -12,9 +12,7 @@ set SKIP_MSI_STEP=1
 set SKIP_PDL_STEP=1
 perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.26.2.1.pp -notest_core -beta=0 -noperl_64bitint -app_simplename=strawberry-perl-no64 -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
 
-set SKIP_MSI_STEP=1
-set SKIP_PDL_STEP=
-perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.26.2.1.pp -notest_core -beta=0 -perl_ldouble -app_simplename=strawberry-perl-ld -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
-
-
-rem XXXXXXXXXXXXXXXX perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.26.2.1.pp -notest_core -beta=0 -perl_ldouble -noperl_64bitint -app_simplename=strawberry-PDL-ld-no64 -nointeractive -restorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
+rem ### no long double build ###
+rem ### set SKIP_MSI_STEP=1
+rem ### set SKIP_PDL_STEP=
+rem ### perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.26.2.1.pp -notest_core -beta=0 -perl_ldouble -app_simplename=strawberry-perl-ld -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
