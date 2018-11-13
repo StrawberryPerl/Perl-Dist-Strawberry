435015b2f95e31fdc8a58e8749db0da3ede5cee4
kmx <kmx@cpan.org>
2018-11-13 09:36:08 +0100

    update openssl + cfitsio

--- share/32bit-5.28.1.1.pp
+++ share/32bit-5.28.1.1.pp
@@ -44,7 +44,8 @@
             'liblibxslt'    => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_libxslt-1.1.29-bin_20170517.zip',
             'libmpc'        => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_mpc-1.0.3-bin_20170517.zip',
             'libmpfr'       => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_mpfr-3.1.5-bin_20170517.zip',
-            'libopenssl'    => '<package_url>/kmx/32_libs/gcc71-2018Q2/32bit_openssl-1.1.0h-bin_20180331.zip',
+          ##'libopenssl'    => '<package_url>/kmx/32_libs/gcc71-2018Q2/32bit_openssl-1.1.0h-bin_20180331.zip',
+            'libopenssl'    => '<package_url>/kmx/32_libs/gcc71-2018Q4/32bit_openssl-1.1.0i-bin_20181113.zip',
             'libpostgresql' => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_postgresql-9.6.3-bin_20170517.zip',
             'libt1lib'      => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_t1lib-5.1.2-bin_20170517.zip',
             'libtiff'       => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_tiff-4.0.7-bin_20170517.zip',
@@ -414,7 +415,7 @@
             'talib'         => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_ta-lib-0.4.0-bin_20170517.zip',
             'netcdf'        => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_netcdf-4.4.1.1-bin_20170517.zip',
             'lapack'        => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_lapack-3.7.0-bin_20170523.zip',
-            'cfitsio'       => '<package_url>/kmx/32_libs/gcc71-2017Q2/32bit_cfitsio-3.41-bin_20170517.zip',
+            'cfitsio'       => '<package_url>/kmx/32_libs/gcc71-2018Q4/32bit_cfitsio-3.45-bin_20181113.zip',
         },
     },
     ### NEXT STEP ###########################
