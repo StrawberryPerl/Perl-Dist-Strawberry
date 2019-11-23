04bc8e567d0fa8fb2cf566023f4a4c3a722b4ee8
kmx <kmx@cpan.org>
2019-11-23 14:12:59 +0100

    version bump

--- lib/Perl/Dist/Strawberry.pm
+++ lib/Perl/Dist/Strawberry.pm
@@ -17,7 +17,7 @@ use File::ShareDir        qw();
 use Pod::Usage            qw(pod2usage);
 use LWP::UserAgent;
 
-our $VERSION = '4.019';
+our $VERSION = '4.020';
 
 sub new {
   my $class = shift;
