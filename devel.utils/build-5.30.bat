2da22abeaa4038174eb3697145a0ae0ef182fe3d
kmx <kmx@cpan.org>
2020-02-07 17:38:23 +0100

    preparing 5.30.2

--- devel.utils/build-5.30.bat
+++ devel.utils/build-5.30.bat
@@ -5,10 +5,10 @@ set PERL_USE_UNSAFE_INC=1
 
 set SKIP_MSI_STEP=
 set SKIP_PDL_STEP=
-perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.30.1.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
-perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.30.1.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
+perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.30.2.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
+perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.30.2.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
 
 set SKIP_MSI_STEP=1
 set SKIP_PDL_STEP=1
-perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.30.1.1.pp -notest_core -beta=0 -noperl_64bitint -app_simplename=strawberry-perl-no64 -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
+perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.30.2.1.pp -notest_core -beta=0 -noperl_64bitint -app_simplename=strawberry-perl-no64 -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38 -cpan_url file:///z:/_cpan-mirror/
 
