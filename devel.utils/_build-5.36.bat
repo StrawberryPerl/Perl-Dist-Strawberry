a104cc9d043b24f28a23cf8662cca2e16b1d920a
Shawn Laffan <shawnlaffan@gmail.com>
2023-03-04 16:23:03 +1100

    5.36 batch file: rebuild blib each time
    Otherwise uptes to the config files and code are missed
    when the user neglects to manually run it.
    
    Can be disabled at a later date.

--- devel.utils/_build-5.36.bat
+++ devel.utils/_build-5.36.bat
@@ -6,6 +6,9 @@
 set SP=z:\sp532
 set PATH=%SP%\c\bin;%SP%\perl\bin;%SP%\perl\site\bin;%PATH%
 
+:: update blib - requires Build.PL to have been run
+..\Build
+
 set SKIP_MSI_STEP=1
 set SKIP_PDL_STEP=1
 perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.36.0.1.pp -test_core -beta=0 -nointeractive -norestorepoints -wixbin_dir=z:\sw\wix311 -cpan_url https://cpan.metacpan.org
