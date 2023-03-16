8b9cb82c44e973ee23b1a5f8f0ee36cd42ed1407
Shawn Laffan <shawnlaffan@gmail.com>
2023-03-16 12:04:36 +1100

    5.36 build script: modify use of sp5.32
    This will hopefully avoid potential overrides of utilities
    from the winlibs version we want to use.

--- devel.utils/_build-5.36.bat
+++ devel.utils/_build-5.36.bat
@@ -4,7 +4,8 @@
 ::set PERL_USE_UNSAFE_INC=1
 
 set SP=z:\sp532
-set PATH=%SP%\c\bin;%SP%\perl\bin;%SP%\perl\site\bin;%PATH%
+set PATH=%PATH%;%SP%\c\bin;%SP%\perl\bin;%SP%\perl\site\bin
+set PERLEXE=%SP%\perl\bin\perl
 
 :: update blib - requires Build.PL to have been run
 set OLD_CD=%cd%
@@ -14,6 +15,6 @@ cd %OLD_CD%
 
 set SKIP_MSI_STEP=1
 set SKIP_PDL_STEP=1
-perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.36.0.1.pp -test_core -beta=0 -nointeractive -norestorepoints -wixbin_dir=z:\sw\wix311 -cpan_url https://cpan.metacpan.org
+%PERLEXE% -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.36.0.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir=z:\sw\wix311 -cpan_url https://cpan.metacpan.org
 
 
