d7142985f28686a4036cc2692efda3d25be9e08d
Chase Whitener <cwhitener@gmail.com>
2024-04-28 04:54:26 +0100

    Add Syntax::Keyword::Try. update share scripts a bit. update cpanm

--- share/utils/CPAN_get_upgrade_list.pl
+++ share/utils/CPAN_get_upgrade_list.pl
@@ -1,10 +1,12 @@
-use 5.012;
+use v5.18;
 use warnings;
 
 use CPAN;
-use Storable              qw(nstore);
-use Data::Dumper          qw(Dumper);
-use Getopt::Long          qw(GetOptions);
+use Storable      qw(nstore);
+use Data::Dumper  qw(Dumper);
+use Getopt::Long  qw(:config gnu_getopt no_ignore_case);
+use Syntax::Keyword::Try;
+require Config;
 
 warn ">> started '$0'\n";
 
@@ -14,26 +16,22 @@ my @spec = (
     'out_dumper=s',
     'out_nstore=s',
 );
-GetOptions(\my %a, @spec) or die ">> invalid option(s)";
+GetOptions(\my %opts, @spec) or die ">> invalid option(s)";
 
-my $out_nstore = $a{out_nstore} // 'upgrade-list.nstore.txt';
-my $out_dumper = $a{out_dumper} // 'upgrade-list.dumper.txt';
-my $url = $a{url} // 'http://cpan.strawberryperl.com';
+my $out_nstore = $opts{out_nstore} // 'upgrade-list.nstore.txt';
+my $out_dumper = $opts{out_dumper} // 'upgrade-list.dumper.txt';
+my $url = $opts{url} // 'http://cpan.strawberryperl.com';
 
 warn Dumper($url);
 CPAN::HandleConfig->load unless $CPAN::Config_loaded++;
 $CPAN::Config->{'urllist'} = [ $url ];
-
-my ($module, %seen, %need);
-my @toget = ();
 
 warn ">> gonna call CPAN::Shell\n";
 CPAN::Shell->reload('index');
 my @modulelist = CPAN::Shell->expand('Module', '/./');
 
 # Schwartzian transform from CPAN.pm.
-my @expand;
-@expand = map {
+my @expand = map {
   $_->[1]
 } sort {
   $b->[0] <=> $a->[0]
@@ -45,73 +43,76 @@ my @expand;
   ]
 } @modulelist;
 
-require Config;
-my $vendorlib=$Config::Config{'installvendorlib'};
+my $vendorlib = $Config::Config{'installvendorlib'};
 
-for $module (@expand) {
-  my $file = $module->cpan_file;
+my (%seen, %need);
+my @toget = ();
+for my $module (@expand) {
+    my $file = $module->cpan_file;
 
-  # If there's no file to download, skip it.
-  next unless defined $file;
+    # If there's no file to download, skip it.
+    next unless defined $file;
 
-  $file =~ s{^./../}{};
-  my $latest  = $module->cpan_version;
-  my $inst_file = $module->inst_file;
-  my $have;
-  my $next_MODULE;
-  eval { # version.pm involved!
-    if ($inst_file and $vendorlib ne substr($inst_file,0,length($vendorlib))) {
-      $have = $module->inst_version;
-      local $^W = 0;
-      if (CPAN::Version->vgt($latest, $have) && !($have eq "undef" && $latest ne "undef")) {
-        #warn "UPGRADE NEEDED: '$inst_file' have=$have latest=$latest\n" if "$have" ne "$latest";
-      }
-      else {
-        ++$next_MODULE 
-      }
-      # to be pedantic we should probably say:
-      #    && !($have eq "undef" && $latest ne "undef" && $latest gt "");
-      # to catch the case where CPAN has a version 0 and we have a version undef
-    } else {
-       ++$next_MODULE;
+    $file =~ s{^./../}{};
+    my $latest  = $module->cpan_version;
+    my $inst_file = $module->inst_file;
+    my $have;
+    my $next_MODULE;
+    try {
+        # version.pm involved!
+        if ($inst_file and $vendorlib ne substr($inst_file, 0, length($vendorlib))) {
+            $have = $module->inst_version;
+            local $^W = 0;
+            if (CPAN::Version->vgt($latest, $have) && !($have eq "undef" && $latest ne "undef")) {
+                #warn "UPGRADE NEEDED: '$inst_file' have=$have latest=$latest\n" if "$have" ne "$latest";
+            } else {
+                ++$next_MODULE 
+            }
+            # to be pedantic we should probably say:
+            #    && !($have eq "undef" && $latest ne "undef" && $latest gt "");
+            # to catch the case where CPAN has a version 0 and we have a version undef
+        } else {
+            ++$next_MODULE;
+        }
     }
-  };
+    catch($e) {
+        next;
+    }
+    next if $next_MODULE;
 
-  next if $next_MODULE;
-  next if ($@);
+    $seen{$file} ||= 0;
+    next if $seen{$file}++;
 
-  $seen{$file} ||= 0;
-  next if $seen{$file}++;
-
-  push @toget, { distribution  => $module->distribution->base_id, 
-                 cpan_file     => $module->cpan_file,
-                 cpan_version  => $module->cpan_version,
-                 local_version => $module->inst_version,
-               };
-  $need{$module->id}++;
+    push @toget, {
+        distribution  => $module->distribution->base_id, 
+        cpan_file     => $module->cpan_file,
+        cpan_version  => $module->cpan_version,
+        local_version => $module->inst_version,
+    };
+    $need{$module->id}++;
 }
 
 ##@toget = sort { $a->distribution cmp $b->distribution } @toget;
-my $rv = { to_upgrade=>\@toget, method=>'CPAN', timestamp=>time };
+my $rv = { to_upgrade => \@toget, method => 'CPAN', timestamp => time };
 
-if (scalar(@toget)==0) {
-  warn ">> All modules are up to date\n";
+if (scalar(@toget) == 0) {
+    warn ">> All modules are up to date\n";
 }
 else {
-  warn ">> ", scalar(@toget), " module(s) need upgrade\n";
-  warn ">> * $_->{cpan_file}\n" for (@toget);
+    warn ">> ", scalar(@toget), " module(s) need upgrade\n";
+    warn ">> * $_->{cpan_file}\n" for (@toget);
 }
 
 if ($out_nstore) {
-  #store via Storable
-  nstore $rv, $out_nstore;
+    #store via Storable
+    nstore $rv, $out_nstore;
 }
 
 if ($out_dumper) {
-  #store via Data::Dumper
-  open my $fh, ">", $out_dumper or die ">> open: $!";
-  print $fh Dumper($rv);
-  close $fh;
+    #store via Data::Dumper
+    open my $fh, ">", $out_dumper or die ">> open: $!";
+    print $fh Dumper($rv);
+    close $fh;
 }
 
 warn ">> Done!\n";
--- share/utils/CPAN_install_module.pl
+++ share/utils/CPAN_install_module.pl
@@ -1,9 +1,9 @@
-use 5.012;
+use v5.18;
 use warnings;
 
 use CPAN;
 use File::Spec::Functions qw(catfile);
-use Getopt::Long;
+use Getopt::Long qw(:config gnu_getopt no_ignore_case);
 
 my @spec = (
     'module=s@',
@@ -18,74 +18,75 @@ my @spec = (
     'internet=i',
     'assume=i',
 );
-my %a = ();
-GetOptions(\%a, @spec) or die ">> invalid option(s)";
+my %opts = ();
+GetOptions(\%opts, @spec) or die ">> invalid option(s)";
 
 #defaults
-$a{module}      //= [];
-$a{install_to}  //= '';
-$a{url}         //= 'http://cpan.strawberryperl.com';
-$a{run_tests}   //= 1,
-$a{follow_deps} //= 1;
+$opts{module}      //= [];
+$opts{install_to}  //= '';
+$opts{url}         //= 'http://cpan.strawberryperl.com';
+$opts{run_tests}   //= 1,
+$opts{follow_deps} //= 1;
 
-$a{internet}    //= 1;
-$a{assume}      //= 0;
-$a{dp_dir}      //= 'c:\temp\xxx';
-$a{dist_file}   //= 'c:\temp\xxx.dist.file.txt';
-$a{output_dir}  //= 'c:\temp\xxx';
-$a{use_sqlite}  //= 0;
-$a{force}       //= 0;
+$opts{internet}    //= 1;
+$opts{assume}      //= 0;
+$opts{dp_dir}      //= 'c:\temp\xxx';
+$opts{dist_file}   //= 'c:\temp\xxx.dist.file.txt';
+$opts{output_dir}  //= 'c:\temp\xxx';
+$opts{use_sqlite}  //= 0;
+$opts{force}       //= 0;
 
 my $module_list = '';
 
 CPAN::HandleConfig->load unless $CPAN::Config_loaded++;
-$CPAN::Config->{'urllist'}     = [ $a{url} ];
-$CPAN::Config->{'use_sqlite'}  = $a{use_sqlite};
-$CPAN::Config->{'prefs_dir'}   = $a{dp_dir};
-$CPAN::Config->{'patches_dir'} = $a{dp_dir};
-if ($a{follow_deps}) {
-  $CPAN::Config->{'prerequisites_policy'} = q[follow];
+$CPAN::Config->{'urllist'}     = [ $opts{url} ];
+$CPAN::Config->{'use_sqlite'}  = $opts{use_sqlite};
+$CPAN::Config->{'prefs_dir'}   = $opts{dp_dir};
+$CPAN::Config->{'patches_dir'} = $opts{dp_dir};
+if ($opts{follow_deps}) {
+    $CPAN::Config->{'prerequisites_policy'} = q[follow];
 }
 else {
-  $CPAN::Config->{'prerequisites_policy'} = q[ignore];
+    $CPAN::Config->{'prerequisites_policy'} = q[ignore];
 }
-$CPAN::Config->{'connect_to_internet_ok'} = $a{internet};
+$CPAN::Config->{'connect_to_internet_ok'} = $opts{internet};
 $CPAN::Config->{'ftp'} = q[];
-if ($a{install_to} =~ /(perl|site|vendor)/) {
-  my $module_build_dirs = $a{install_to};
-  $module_build_dirs = 'core' if $module_build_dirs eq 'perl';
-  $CPAN::Config->{'makepl_arg'}         = "INSTALLDIRS=$a{install_to}";
-  $CPAN::Config->{'make_install_arg'}   = "INSTALLDIRS=$a{install_to}";
-  $CPAN::Config->{'mbuildpl_arg'}       = "--installdirs $module_build_dirs";
-  $CPAN::Config->{'mbuild_install_arg'} = "--installdirs $module_build_dirs";
+if ($opts{install_to} =~ /(perl|site|vendor)/) {
+    my $module_build_dirs = $opts{install_to};
+    $module_build_dirs = 'core' if $module_build_dirs eq 'perl';
+    $CPAN::Config->{'makepl_arg'}         = "INSTALLDIRS=$opts{install_to}";
+    $CPAN::Config->{'make_install_arg'}   = "INSTALLDIRS=$opts{install_to}";
+    $CPAN::Config->{'mbuildpl_arg'}       = "--installdirs $module_build_dirs";
+    $CPAN::Config->{'mbuild_install_arg'} = "--installdirs $module_build_dirs";
 }
-open(my $cpan_fh, '>', $a{dist_file}) or die ">> open: $!";
+open(my $cpan_fh, '>', $opts{dist_file}) or die ">> open: $!";
 MODULE:
-foreach my $name (@{$a{module}}) {
-  warn ">> Installing $name from CPAN...\n";
-  my $module = CPAN::Shell->expandany($name) or die ">> CPAN.pm couldn't locate $name";
+foreach my $name (@{$opts{module}}) {
+    warn ">> Installing $name from CPAN...\n";
+    my $module = CPAN::Shell->expandany($name) or die ">> CPAN.pm couldn't locate $name";
 
-  if ( $module->uptodate() ) {
-    unlink $a{dist_file};
-    warn ">> $name is up to date\n";
-    say $cpan_fh "$name;;;" or die ">> say: $!";
-    next MODULE;
-  }
-  eval {
-    if ($a{run_tests} == 1) {
-      CPAN::Shell->install($name);
+    if ( $module->uptodate() ) {
+        unlink $opts{dist_file};
+        warn ">> $name is up to date\n";
+        say $cpan_fh "$name;;;" or die ">> say: $!";
+        next MODULE;
     }
-    elsif ($a{run_tests} == 2) {
-      CPAN::Shell->force('install', $name);
-    }
-    elsif ($a{run_tests} == 0) {
-      CPAN::Shell->notest('install', $name);
-    }
-    else {
-      die "invalid run_tests";
-    }
-  };
-  my $error = $@;
+
+    try {
+        if ($opts{run_tests} == 1) {
+            CPAN::Shell->install($name);
+        }
+        elsif ($opts{run_tests} == 2) {
+            CPAN::Shell->force('install', $name);
+        }
+        elsif ($opts{run_tests} == 0) {
+            CPAN::Shell->notest('install', $name);
+        }
+        else {
+            die "invalid run_tests";
+        }
+    } catch($e) {
+        my $error = $@;
        
        #XXX-FIXME probably not needed
        #my $id = $module->distribution()->pretty_id();
@@ -97,9 +98,10 @@ foreach my $name (@{$a{module}}) {
        #die "Installation of $name failed: $error\n" if $error;
        #say $cpan_fh "$name;$id;$filename;"  or die "say: $!";
        
-  warn ">> Completed install of '$name'\n";
-  warn ">> ERROR=$error" if $error;
-  die ">> Installation of '$name' appears to have failed\n" unless $a{assume} or $module->uptodate();
+        warn ">> ERROR=$error" if $error;
+    }
+    warn ">> Completed install of '$name'\n";
+    die ">> Installation of '$name' appears to have failed\n" unless $opts{assume} or $module->uptodate();
 }
 close $cpan_fh or die ">> close: $!";
 exit 0;
