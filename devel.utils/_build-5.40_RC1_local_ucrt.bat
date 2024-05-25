6609adc277d613c8c4953acb03255a49799744c7
Shawn Laffan <shawnlaffan@gmail.com>
2024-05-25 13:45:25 +1000

    Config files for 5.40.0.1 RC1

---
+++ devel.utils/_build-5.40_RC1_local_ucrt.bat
@@ -0,0 +1,28 @@
+::@cls
+::call ..\build.bat test
+
+::set PERL_USE_UNSAFE_INC=1
+
+subst Z: C:\shawn\sp_repos
+
+set SP=c:\perls\5.38.2.2_PDL
+set PATH=%SP%\c\bin;%SP%\perl\bin;%SP%\perl\site\bin;Z:\sw\wix311;Z:\winlibs\bin;%PATH%
+set PERLEXE=%SP%\perl\bin\perl
+
+:: update blib - requires Build.PL to have been run
+::set OLD_CD=%cd%
+::cd ..
+::call Build
+::cd %OLD_CD%
+
+call ..\build.bat test
+
+set MAKEFLAGS=-j8
+set TEST_JOBS=8
+
+
+::set SKIP_MSI_STEP=1
+::set SKIP_PDL_STEP=1
+%PERLEXE% -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.40.1.1_RC1.pp -test_core -beta=0 -interactive -restorepoints -wixbin_dir=z:\sw\wix311 -cpan_url https://cpan.metacpan.org
+
+
